36ef723b4a19793800c04f5207d69cbb5903225c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-12-29 14:58:27 +0900

    miscs
    ---------
    d.bat --> edited

--- d.bat
+++ d.bat
@@ -4,7 +4,13 @@ taskkill /f /im gimp-2.8.exe
 
 taskkill /f /im wish.exe
 
-taskkill /f /im blender.exe
-taskkill /f /im blender-app.exe
+taskkill /f /im MovieMaker.exe
+
+taskkill /f /im audacity.exe
+
+taskkill /f /im wmplayer.exe
+
+REM taskkill /f /im blender.exe
+REM taskkill /f /im blender-app.exe
 
 exit
